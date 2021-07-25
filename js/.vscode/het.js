b116c091fe43b45940696fe1a5ae29eddbeea880
het <het66068@gmail.com>
2021-07-25 13:53:54 +0530

    fix the bug

---
+++ js/.vscode/het.js
@@ -0,0 +1,34 @@
+// const age=35;
+
+// if(age!=29)
+// {
+//   console.log('age is  not 29')
+  
+// }
+
+// if(age!==35)
+// {
+//   console.log('age is not  35')
+// }
+
+
+// else{
+//   console.log('age is not 19')
+
+// for(let i=0;i<100;i++)
+// {
+//   console.log(i);
+// }
+// let j=1;
+// while (j<10) {
+//    console.log(j);
+//    j++;
+
+
+let elemclass=document.getElementsByClassName('container')
+console.log(elemclass);
+
+
+
+
+
---
+++ js/.vscode/index.js
@@ -0,0 +1,237 @@
+//console.table
+//console.time ('your time is took')  console.timeEnd('your time is took');
+//console.assert('assambility this is not possible');
+// console.error('this is an error');
+/*
+most cooman proggraming case type
+
+1.camelcase = firstcapital
+2.Pascal case
+*/
+
+/*
+premetive data typa=string,number,bullian,null,undifind,symbol;
+refreance data type=arrays,object,functions,date;
+*/
+
+
+// parseInt ,parseFloat is number value
+//  tofixed value is represent desimle number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.time('time khatam chutiye');
+
+//console.log([1,2,3,])
+//console.log({this:'het',marks:'34'})
+//console.table({this:'het',marks:'34'})
+//console.warn('this is a warnning');
+
+//console.timeEnd('time khatam chutiye');
+//console.assert(565<160, 'Age>160 this is not posssible');
+
+/*
+let city="dilhi";
+console.log(city);
+
+const ownername="harir ram";
+//ownername="hari"
+console.log(ownername);
+
+{
+    let city="rampur";
+    console.log(city);
+
+}
+
+//console.log(city);
+
+//numbers
+/*
+let marks=34;
+console.log("data type is" + typeof(marks));
+
+
+let isdrive=true;
+console.log("data type is" + typeof(true));
+
+let undef= undefined;
+console.log("data type is" + typeof(undef));
+
+//refrence data type
+ myarr=[1,2,3,4,true,"gutt",34];
+ console.log("data type is" + typeof(myarr));
+
+ let stmarks= {
+     harry:34,
+     shubham:36,
+     hetcode:89
+ }
+ console.log( typeof stmarks);
+
+
+    function myname() {
+        
+    } 
+ console.log( typeof myname);
+
+let date= new Date();
+console.log(typeof date);
+
+*/
+
+/*
+let myvar;
+myvar= String(34);
+console.log(myvar,typeof (myvar));
+
+
+let booleanvar =String(true);
+console.log(booleanvar,typeof (booleanvar));
+
+let date = String( new Date());
+console.log( date,typeof (date));
+
+let arr= [1,2,3,4,5];
+console.log(arr.length,typeof(arr));
+*/
+
+
+//     let i=75;
+//    // console.log(i.toString())
+
+     
+// let stri= Number( "345");
+// console.log(stri,typeof(stri));
+    
+
+// let number=parseFloat("34");
+// console.log(number.toFixed(30),typeof(number));
+
+// let html;
+// html="<h1>this is madarchod</h1>"+
+//       " <p>haa Bhosdike  </P>"
+
+//       html=html.concat('this',    ' str2 ');
+//       console.log(html)
+//       console.log(html.length);
+//       console.log(html.toLowerCase());
+//       console.log(html.toUpperCase());
+//       console.log(html);
+
+
+//       console.log(html[1])
+//     console.log(html.indexOf('xxx'))
+
+//     console.log(html[0]);
+//     console.log(html.lastIndexOf('<'));
+//     console.log(html.charAt(10));
+//     console.log(html.endsWith(' str2 '))
+//     console.log(html.includes(' str2 '))
+//     console.log(html.substring(2,4))
+//     console.log(html.slice(-4))
+//     console.log(html.split('<'))
+//     console.log(html.replace('this','voh'))
+
+
+
+
+// const name="het";
+// const greatting="good morning"  ;
+// console.log(greatting+ ' '  +name);
+
+// let name="het";
+// let pussi1="pink";
+// let pussi2="red";
+
+// let myhtml=`hello ${name}
+//     <h1>story about sexy puusy </h1>
+//     <p>i like ${pussi1} and
+//      like ${pussi2} pussy `;
+                 
+// document.body.innerHTML=myhtml;
+// let marks=[34,45,67,89];
+//  const frutis=["orange","Apple","pineapple"];
+//  const mixed=["str",67,[3,5]];
+
+//  const arr= new Array(23,45,67,78);
+//  console.log(arr);
+//  console.log(frutis);
+//  console.log(mixed);
+//  console.log(marks);
+
+//  console.log(arr.length);
+//  console.log(Array.isArray(arr));
+
+// arr[0]='het';
+// let arrelement=arr[0]
+// // console.log( arrelement);
+
+// console.log(arr)
+
+// let value= marks.indexOf(67);
+// console.log(value)
+
+//mutating modifys arrays
+// marks.push(34);
+// console.log(marks)
+
+// marks.unshift(34);
+// console.log(marks)
+
+
+// marks.pop();
+// console.log(marks)
+
+// marks.shift()
+// console.log(marks)
+
+// marks.splice(1,2)
+// console.log(marks);
+
+// marks.reverse()
+// console.log(marks)
+
+// let marks2=[1,2,3,4,5]
+// marks=marks.concat(marks2);
+// console.log(marks);
+
+
+// let myobj={
+//  name:"het",
+//  gendor:'male',
+//  okat:'sexy',
+//  angry:true,
+//  number:111,
+
+// }
+// console.log(myobj);
+// console.log(myobj['angry']);
+
+
+
+
+
+
+
+
+
+
+
+
+// html Tags 
+// SourceBuffer.unsplash.com
+
+// ul attribute = disk , square,circle
